2fb0ae83d993b4b547ffaa99fa074aa2c8216c38
Elizabeth Prins <elizabeth@strangelove.nl>
2018-02-07 13:48:00 +0100

    switch localhost out for localhost or IP, for mobile devices

--- src/api/client.js
+++ src/api/client.js
@@ -6,7 +6,7 @@ export default class ApiClient {
   }
 
   constructor(host, options = {}) {
-    this.host = host || 'http://localhost:3030'
+    this.host = host || ('http://' + window.location.hostname + ':3030')
     this.options = { ...this.defaultOptions, ...options }
   }
 
